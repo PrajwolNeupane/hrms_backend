29e57e19711c148cb529b5ce5559049542431cc4
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-03-29 22:56:12 +0545

    upd:changed db env

--- dist/config/dbConnection.js
+++ dist/config/dbConnection.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 require("dotenv/config");
 exports.default = mongoose_1.default
-    .connect(`${process.env.DB_URL}`)
+    .connect(`${process.env.MONGODB_URI}`)
     .then(() => {
     console.log("Connected to Mongo DB");
 })
