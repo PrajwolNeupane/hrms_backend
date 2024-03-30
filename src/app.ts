ce0a08ca7d29d408759a38671479580a737b43a2
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-03-30 13:20:45 +0545

    upd:added cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from "express";
 import auth from "./routes/auth";
 import attendanceRoutes from "./routes/attendance";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
